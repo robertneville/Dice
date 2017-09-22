3150d5c5826edb1d6e1138250d6c94ffd54ecea0
Robert <ran4@students.waikato.ac.nz>
2017-09-22 12:55:56 +1200

    changed labels to buttons. added more buttons. added listeners to new buttons

--- app/src/main/java/com/example/robert/dice/FragmentGame.java
+++ app/src/main/java/com/example/robert/dice/FragmentGame.java
@@ -4,38 +4,30 @@ package com.example.robert.dice;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class FragmentGame extends Fragment {
+    int mRollNumber = 3;
     //the scores
     TextView mOnes,mTwos,mThrees,mFours,mFives,mSixes,m3kind,m4kind,mSmStraight,mLgStraight,
             mFullHouise,mChance,m5Kind;
     //labels that you will be able to press to enter a score into the scores
-    TextView mOnesLabel,mTwosLabel,mThreesLabel,mFoursLabel,mFivesLabel,mSixesLabel,m3kindLabel,
+    Button mOnesLabel,mTwosLabel,mThreesLabel,mFoursLabel,mFivesLabel,mSixesLabel,m3kindLabel,
             m4kindLabel,mSmStraightLabel,mLgStraighLabel,mFullHouiseLabel,mChanceLabel,m5KindLabel;
     //cup to roll
-    Button cup;
+    ImageButton mCup;
     //dice
     Button mDice1Keep, mDice2Keep, mDice3Keep, mDice4Keep, mDice5Keep, mDice1Roll, mDice2Roll,
             mDice3Roll, mDice4Roll, mDice5Roll;
 
     public FragmentGame() {
         // Required empty public constructor
-    }
-
-    /**
-     *
-     * @param savedInstanceState
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -88,7 +80,7 @@ public class FragmentGame extends Fragment {
      * @param view The layout holding all these widgets.
      */
     private void InstantiateScoreLabels(View view) {
-        mOnesLabel = (TextView) view.findViewById(R.id.label_score_1);
+        mOnesLabel = (Button) view.findViewById(R.id.label_score_1);
         mOnesLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -96,7 +88,7 @@ public class FragmentGame extends Fragment {
                Toast.makeText(getActivity(),"Pressed Ones Label", Toast.LENGTH_SHORT).show();
             }
         });
-        mTwosLabel = (TextView) view.findViewById(R.id.label_score_2);
+        mTwosLabel = (Button) view.findViewById(R.id.label_score_2);
         mTwosLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -104,7 +96,7 @@ public class FragmentGame extends Fragment {
                 Toast.makeText(getActivity(),"Pressed Twos Label", Toast.LENGTH_SHORT).show();
             }
         });
-        mThreesLabel = (TextView) view.findViewById(R.id.label_score_3);
+        mThreesLabel = (Button) view.findViewById(R.id.label_score_3);
         mThreesLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -112,7 +104,7 @@ public class FragmentGame extends Fragment {
                 Toast.makeText(getActivity(),"Pressed Threes Label", Toast.LENGTH_SHORT).show();
             }
         });
-        mFoursLabel = (TextView) view.findViewById(R.id.label_score_4);
+        mFoursLabel = (Button) view.findViewById(R.id.label_score_4);
         mFoursLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -120,7 +112,7 @@ public class FragmentGame extends Fragment {
                 Toast.makeText(getActivity(),"Pressed Fours Label", Toast.LENGTH_SHORT).show();
             }
         });
-        mFivesLabel = (TextView) view.findViewById(R.id.label_score_5);
+        mFivesLabel = (Button) view.findViewById(R.id.label_score_5);
         mFivesLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -128,7 +120,7 @@ public class FragmentGame extends Fragment {
                 Toast.makeText(getActivity(),"Pressed Fives Label", Toast.LENGTH_SHORT).show();
             }
         });
-        mSixesLabel = (TextView) view.findViewById(R.id.label_score_6);
+        mSixesLabel = (Button) view.findViewById(R.id.label_score_6);
         mSixesLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -136,7 +128,7 @@ public class FragmentGame extends Fragment {
                 Toast.makeText(getActivity(),"Pressed Sixes Label", Toast.LENGTH_SHORT).show();
             }
         });
-        m3kindLabel = (TextView) view.findViewById(R.id.label_score_3kind);
+        m3kindLabel = (Button) view.findViewById(R.id.label_score_3kind);
         m3kindLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -144,7 +136,7 @@ public class FragmentGame extends Fragment {
                 Toast.makeText(getActivity(),"Pressed 3 of Kind Label", Toast.LENGTH_SHORT).show();
             }
         });
-        m4kindLabel = (TextView) view.findViewById(R.id.label_score_4kind);
+        m4kindLabel = (Button) view.findViewById(R.id.label_score_4kind);
         m4kindLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -152,7 +144,7 @@ public class FragmentGame extends Fragment {
                 Toast.makeText(getActivity(),"Pressed 4 of Kind Label", Toast.LENGTH_SHORT).show();
             }
         });
-        mSmStraightLabel = (TextView) view.findViewById(R.id.label_score_smstraight);
+        mSmStraightLabel = (Button) view.findViewById(R.id.label_score_smstraight);
         mSmStraightLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -160,7 +152,7 @@ public class FragmentGame extends Fragment {
                 Toast.makeText(getActivity(),"Pressed Small Straight Label", Toast.LENGTH_SHORT).show();
             }
         });
-        mLgStraighLabel = (TextView) view.findViewById(R.id.label_score_lgstraight);
+        mLgStraighLabel = (Button) view.findViewById(R.id.label_score_lgstraight);
         mLgStraighLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -168,7 +160,7 @@ public class FragmentGame extends Fragment {
                 Toast.makeText(getActivity(),"Pressed Large Straight Label", Toast.LENGTH_SHORT).show();
             }
         });
-        mFullHouiseLabel = (TextView) view.findViewById(R.id.label_score_fullhouse);
+        mFullHouiseLabel = (Button) view.findViewById(R.id.label_score_fullhouse);
         mFullHouiseLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -176,7 +168,7 @@ public class FragmentGame extends Fragment {
                 Toast.makeText(getActivity(),"Pressed Full House Label", Toast.LENGTH_SHORT).show();
             }
         });
-        mChanceLabel = (TextView) view.findViewById(R.id.label_score_chance);
+        mChanceLabel = (Button) view.findViewById(R.id.label_score_chance);
         mChanceLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -184,7 +176,7 @@ public class FragmentGame extends Fragment {
                 Toast.makeText(getActivity(),"Pressed Chance Label", Toast.LENGTH_SHORT).show();
             }
         });
-        m5KindLabel = (TextView) view.findViewById(R.id.label_score_5kind);
+        m5KindLabel = (Button) view.findViewById(R.id.label_score_5kind);
         m5KindLabel.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -201,7 +193,108 @@ public class FragmentGame extends Fragment {
      */
     private void InstantiateButtons(View view) {
 
+        mDice1Keep = (Button)view.findViewById(R.id.gameDiceKeep1);
+        mDice1Keep.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Pressed Keep 1 Dice", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mDice2Keep = (Button)view.findViewById(R.id.gameDiceKeep2);
+        mDice2Keep.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Pressed Keep 2 Dice", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mDice3Keep = (Button)view.findViewById(R.id.gameDiceKeep3);
+        mDice3Keep.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Pressed Keep 3 Dice", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mDice4Keep = (Button)view.findViewById(R.id.gameDiceKeep4);
+        mDice4Keep.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Pressed Keep 4 Dice", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mDice5Keep = (Button)view.findViewById(R.id.gameDiceKeep5);
+        mDice5Keep.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Pressed Keep 5 Dice", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mDice1Roll = (Button)view.findViewById(R.id.gameDiceRoll1);
+        mDice1Roll.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Pressed Roll 1 Dice", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mDice2Roll = (Button)view.findViewById(R.id.gameDiceRoll2);
+        mDice2Roll.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Pressed Roll 2 Dice", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mDice3Roll = (Button)view.findViewById(R.id.gameDiceRoll3);
+        mDice3Roll.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Pressed Roll 3 Dice", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mDice4Roll = (Button)view.findViewById(R.id.gameDiceRoll4);
+        mDice4Roll.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Pressed Roll 4 Dice", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mDice5Roll = (Button)view.findViewById(R.id.gameDiceRoll5);
+        mDice5Roll.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Pressed Roll 5 Dice", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mCup = (ImageButton)view.findViewById(R.id.game_cup);
+        mCup.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                StartRoll(); 
+            }
+        });
     }
 
+
+    private void StartTurn() {
+        mRollNumber = 3;
+
+    };
+
+    private void StartRoll() {
+        if(mRollNumber>0 && mRollNumber < 4) {
+            Toast.makeText(getActivity(), "No more rolls. Select a score.", Toast.LENGTH_LONG).show();
+        }
+
+    }
 
 }
